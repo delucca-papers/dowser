7c10568c2c228e43dab3abffaa2f405b8fe20e5b
delucca <gpg@delucca.dev>
2023-07-07 18:25:53 -0300

    feat(experiments): adds new experiment

--- experiments/003-memory-pressure/src/evaluate.py
+++ experiments/003-memory-pressure/src/evaluate.py
@@ -19,11 +19,16 @@ def run(args):
         ],
     )
 
-    max_memory_used = []
-    min_memory_used = []
+    __extract_min_max_memory_usage(df)
+    __extract_time_elapsed(df)
+
+
+def __extract_min_max_memory_usage(df):
     shapes = df["dimension_z"].unique()
     memory_used_per_z = df.groupby(["dimension_z"])["memory_used"].apply(list)
 
+    max_memory_used = []
+    min_memory_used = []
     for result in memory_used_per_z:
         max_memory_used.append(max(result))
         min_memory_used.append(min(result))
@@ -33,7 +38,55 @@ def run(args):
     plt.xlabel("Input shape")
     plt.ylabel("Memory usage (KB)")
     plt.legend()
-    plt.savefig(f"{args.dirpath}/result.png")
+    plt.savefig(f"{args.dirpath}/min-max-memory-usage.png")
+    plt.clf()
+
+
+def __extract_time_elapsed(df):
+    time_elapsed_per_z = df.groupby(["dimension_z"])["elapsed_time"].apply(list)
+    memory_pressure_per_z = df.groupby(["dimension_z"])["memory_pressure"].apply(list)
+    shapes = df["dimension_z"].unique()
+
+    fig, ax1 = plt.subplots()
+    color = "tab:red"
+    ax1.set_xlabel("Memory pressure (%)")
+    ax1.set_ylabel("Time elapsed (s) - (first 3)", color=color)
+    ax1.tick_params(axis="y", labelcolor=color)
+
+    for i, time_elapsed in enumerate(time_elapsed_per_z[:3]):
+        memory_pressure = [
+            pressure * 100 for pressure in memory_pressure_per_z.iloc[:3].iloc[i]
+        ]
+
+        ax1.plot(
+            memory_pressure,
+            time_elapsed,
+            label=f"Shape {shapes[i]}",
+            marker="o",
+            color=color,
+        )
+
+    color = "tab:blue"
+    ax2 = ax1.twinx()
+    ax2.set_ylabel("Time elapsed (s) - (last 3)", color=color)
+    ax2.tick_params(axis="y", labelcolor=color)
+
+    for i, time_elapsed in enumerate(time_elapsed_per_z[-3:]):
+        memory_pressure = [
+            pressure * 100 for pressure in memory_pressure_per_z.iloc[-3:].iloc[i]
+        ]
+
+        ax2.plot(
+            memory_pressure,
+            time_elapsed,
+            label=f"Shape {shapes[i]}",
+            marker="o",
+            color=color,
+        )
+
+    fig.tight_layout()
+    plt.savefig(f"{args.dirpath}/time-elapsed.png")
+    plt.clf()
 
 
 if __name__ == "__main__":
--- experiments/README.md
+++ experiments/README.md
@@ -4,4 +4,4 @@ This folder contain all available experiments related to Dowser.
 
 - [**001 - Hilbert Transform**](./001-hilber-transform): Evaluates the memory usage relationship with the input shape for the Hilbert Transform function;
 - [**002 - Memory usage correctness**](./002-memory-usage-correctness): Evaluate the correctness of our memory usage report strategy;
-- [**003 - Memory pressure][./003-memory-pressure]: Evaluates the impact of adding memory pressure while executing an algorithm.
+- [**003 - Memory pressure**](./003-memory-pressure): Evaluates the impact of adding memory pressure while executing an algorithm.
